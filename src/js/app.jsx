aba6ffb10330273b56dbb350a40a3653fb2579b2
Pooja <poojakmalvi@gmail.com>
2017-09-25 19:35:48 +0530

    Added reducer and store

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
 import Main from './container.jsx';
 
+
+let store = createStore(todoApp);
 
 render(
   <Main/>,
