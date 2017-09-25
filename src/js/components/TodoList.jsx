2f017cf190fa96b2539f493a9703974d53634303
Pooja <poojakmalvi@gmail.com>
2017-09-25 13:54:36 +0530

    Webpack-dev-server Setup

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import TodoItem from './Todo.jsx';
 
 const TodoList = React.createClass({
 	render: function(){
-		return <div>TodoList</div>;
+		return (
+			<div>TodoList</div>
+
+			);
 	}});
 
 export default TodoList;
